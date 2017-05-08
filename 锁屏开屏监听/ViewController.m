df23029608432c9c96931e842a831e7c7bda9e9b
Qianrun <Qianrun@QianrundeMini.lan>
2017-05-08 11:26:08 +0800

    first

--- 锁屏开屏监听/ViewController.m
+++ 锁屏开屏监听/ViewController.m
@@ -8,6 +8,12 @@
 
 #import "ViewController.h"
 
+#include <notify.h>
+
+
+bool screenLocked;
+bool isScreenLocked;
+
 @interface ViewController ()
 
 @end
@@ -16,8 +22,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //锁屏
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, handleLockStateNotification, CFSTR("com.apple.springboard.lockstate"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+    
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, handleDisplayStatusNotification, CFSTR("com.apple.iokit.hid.displayStatus"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
 }
+
+static void handleLockStateNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+    uint64_t state;
+    int token;
+    notify_register_check("com.apple.springboard.lockstate", &token);
+    notify_get_state(token, &state);
+    notify_cancel(token);
+    if ((uint64_t)1 == state)
+    {
+        isScreenLocked = true;
+    }
+    else
+    {
+        screenLocked = false;
+        isScreenLocked = false;
+    }
+}
+
+static void handleDisplayStatusNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+    if (userInfo)
+    {
+        CFShow(userInfo);
+    }
+    uint64_t state;
+    int token;
+    notify_register_check("com.apple.iokit.hid.displayStatus", &token);
+    notify_get_state(token, &state);
+    notify_cancel(token);
+    if ((uint64_t)1 == state)
+    {
+        screenLocked = true;
+    }
+    else
+    {
+        screenLocked = false;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
